4bc35a23bbfeb8fa3951d8aec39e76d94b9f5fc1
fabian7593 <fabianr7593@gmail.com>
2025-06-17 23:50:49 -0600

    fix routes

--- src/modules/notification/index.ts
+++ src/modules/notification/index.ts
@@ -1,9 +1,9 @@
 export { Notification } from "@entity/Notification";
 export { UserNotification } from "@entity/UserNotification";
 export { User } from "@TenshiJS/entity/User";
-export { default as NotificationDTO } from "@modules/01_General/notification/dtos/NotificationDTO";
-export { default as UserNotificationDTO } from "@modules/01_General/notification/dtos/UserNotificationDTO";
-export { default as UserNotificationController } from "@modules/01_General/notification/controllers/UserNotificationController";
-export { requiredBodyList as requiredBodyListNotifications } from "@modules/01_General/notification/validations/NotificationValidations";
-export { requiredBodyList as requiredBodyListUserNotifications } from "@modules/01_General/notification/validations/UserNotificationValidations";
-export { regexValidationList } from "@modules/01_General/notification/validations/NotificationValidations";
+export { default as NotificationDTO } from "@modules/notification/dtos/NotificationDTO";
+export { default as UserNotificationDTO } from "@modules/notification/dtos/UserNotificationDTO";
+export { default as UserNotificationController } from "@modules/notification/controllers/UserNotificationController";
+export { requiredBodyList as requiredBodyListNotifications } from "@modules/notification/validations/NotificationValidations";
+export { requiredBodyList as requiredBodyListUserNotifications } from "@modules/notification/validations/UserNotificationValidations";
+export { regexValidationList } from "@modules/notification/validations/NotificationValidations";
